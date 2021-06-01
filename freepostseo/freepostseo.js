aff818b9b240d14ae907037a21a318557ff1fe91
pranaya1102 <68726192+pranaya1102@users.noreply.github.com>
2021-06-02 00:30:26 +0530

    Add files via upload

---
+++ freepostseo/freepostseo.js
@@ -0,0 +1,50 @@
+
+  	function fun_register() {
+  		document.getElementById('register').style.display = "block ";
+		  document.getElementById('log').style.display = "none";
+  		document.getElementById('login').style.display = "none";
+  		document.getElementById('tools').style.display = "none";
+  		document.getElementById('about').style.display = "none";
+       
+		w3_close();
+        document.documentElement.scrollTop = 0;
+  	}
+  	function fun_login() {
+		
+  		document.getElementById('login').style.display = "block ";
+		  document.getElementById('log').style.display = "block";
+  		document.getElementById('tools').style.display = "none";
+  		document.getElementById('about').style.display = "none";
+  		document.getElementById('register').style.display = "none";
+		w3_close();
+		document.documentElement.scrollTop = 0;
+
+  	}
+  	function fun_home() {
+  		document.getElementById('tools').style.display = "block ";
+		document.getElementById('about').style.display = "block ";
+
+  		document.getElementById('login').style.display = "none";
+  		document.getElementById('register').style.display = "none";
+		  w3_close();
+		document.documentElement.scrollTop = 0;  
+   	}
+	
+	   // Toggle between showing and hiding the sidebar when clicking the menu icon
+	
+
+	function w3_open() {
+		var mySidebar = document.getElementById("mySidebar");
+	if (mySidebar.style.display === 'block') {
+		mySidebar.style.display = 'none';
+		
+	} else {
+		mySidebar.style.display = 'block';
+	}
+	}
+
+	// Close the sidebar with the close button
+	function w3_close() {
+		var mySidebar = document.getElementById("mySidebar");
+		mySidebar.style.display = "none";
+	}
